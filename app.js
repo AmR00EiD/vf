862d6736ad7effdd11af224bb5579db023decf2e
amreid <amrmessi371@gmail.com>
2023-11-15 21:49:27 +0200

    new dark mode

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 /////////////////////// dark mode /////////////////////////////
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   // Get the checkbox element
+//   let checkbox = document.querySelector("input");
+
+//   // Get the body tag
+//   let body = document.querySelector("body");
+
+//   // This event handler listens for the checkbox to be checked or unchecked
+//   // Then, if the checkbox is checked (event.target.checked == true)
+//   // Then apply `data-theme="dark"` to the body tag
+//   // Otherwise, remove the attribute
+//   checkbox.addEventListener("change", (event) => {
+//     localStorage.setItem("checkbox", this.checked);
+//     event.target.checked
+//       ? body.setAttribute("data-theme", "dark")
+//       : body.removeAttribute("data-theme");
+//   });
+// });
+// Get the checkbox element
+let checkbox = document.querySelector("input");
+
+// Get the body tag
+let body = document.querySelector("body");
+
+// This function swaps the data-theme attribute of the HTML tag
+function swapTheme(theme) {
+  body.setAttribute("data-theme", theme);
+}
+
+// This event handler listens for the checkbox to be checked or unchecked
+// Then, it stores the data-theme value in the local storage
+// And calls the swapTheme function with the appropriate value
+checkbox.addEventListener("change", (event) => {
+  localStorage.setItem("data-theme", event.target.checked ? "dark" : "light");
+  swapTheme(event.target.checked ? "dark" : "light");
+});
+
+// This event handler listens for the page to be loaded
+// Then, it gets the data-theme value from the local storage
+// And calls the swapTheme function with the appropriate value
 window.addEventListener("DOMContentLoaded", (event) => {
-  // Get the checkbox element
-  let checkbox = document.querySelector("input");
-
-  // Get the body tag
-  let body = document.querySelector("body");
-
-  // This event handler listens for the checkbox to be checked or unchecked
-  // Then, if the checkbox is checked (event.target.checked == true)
-  // Then apply `data-theme="dark"` to the body tag
-  // Otherwise, remove the attribute
-  checkbox.addEventListener("change", (event) => {
-    event.target.checked
-      ? body.setAttribute("data-theme", "dark")
-      : body.removeAttribute("data-theme");
-  });
+  let theme = localStorage.getItem("data-theme") || "light";
+  swapTheme(theme);
+  checkbox.checked = theme === "dark";
 });
 
 /// password hide and show password ////
